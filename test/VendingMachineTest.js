b5ef4e51f6f39df9a9a0fa6c2cbf088074341afc
Sean Morris <sean@seanmorr.is>
2019-10-14 07:41:26 -0400

    Derived injections.

--- test/VendingMachineTest.js
+++ test/VendingMachineTest.js
@@ -12,13 +12,17 @@ import { BagOfPretzels } from './BagOfPretzels';
 import { VendingMachine } from './VendingMachine';
 
 export class VendingMachineTest extends Inject(Test
-, {vendingMachine:  VendingMachine}
-, ({vendingMachine: VendingMachine})=>({
+
+	, {vendingMachine:  VendingMachine}
+
+	, ({vendingMachine: VendingMachine})=>({
 		sodaMachine:   Inject(VendingMachine, {Drink:Soda})
 		, waterMachine:  Inject(VendingMachine, {Drink:BottleOfWater})
 		, chipMachine:   Inject(VendingMachine, {Snack:BagOfChips})
 		, pretzelMachine:Inject(VendingMachine, {Snack:BagOfPretzels})
-})){
+	})
+
+){
 	testGetDrink()
 	{
 		this.assert(
